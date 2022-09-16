7434b3c1f3243bb900dd23007b77b0ab0cffddc8
Ashley Muston <armuston@gmail.com>
2022-09-16 18:27:18 -0400

    Starting endpoints

--- java/src/main/java/com/techelevator/service/BeerService.java
+++ java/src/main/java/com/techelevator/service/BeerService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -22,18 +23,29 @@ public class BeerService {
     private final BeerRepository beerRepo;
     @Autowired
     private final BreweryRepository breweryRepo;
-    @Autowired //Necessary to autowire?
+    @Autowired // Necessary to autowire?
     private final UserDao userDao;
 
+    public List<Beer> getAllBeers() {
+        return beerRepo.findAll();
+    }
+
     public List<Beer> getBeersByBrewer(Principal principal) {
-        User currentUser = userDao.findByUsername(principal.getName());
+        // Optional<Brewery> userBrewery = breweryRepo.findById(principal);
+        // return currentUser.getBrewery().getBeers();
 
-//        return currentUser.getBrewery().getBeers();
-
-        //get current user id
+        // get current user id
         // get brewery that matches user id
         // get all beers that match brewery id
         return null;
     }
 
+    public Optional<Beer> getBeerById(Long id) {
+        return beerRepo.findById(id);
+    }
+
+    public Beer addBeer(Beer newBeer) {
+        beerRepo.add(newBeer);
+        return newBeer;
+    }
 }
